f3a6242b47c86d80e5bfd3c15d0fa3644824394c
patrickstar <wxiaowar@gmail.com>
2025-06-12 15:57:33 +0800

    add page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function Dashboard({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  // const id = (await params).id;
+
+  return (
+    <>
+      link page - <p></p>
+    </>
+  );
+}
---
+++ src/app/link/[id]/page.tsx
@@ -0,0 +1,13 @@
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+
+  return (
+    <>
+      link page - <p>{id} 11</p>
+    </>
+  );
+}
